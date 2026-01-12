40810978d1d2733ea73330d0b7bf8f9e2d7cab6d
Earl Turner <earl.e.turner@outlook.com>
2026-01-12 14:17:43 -0600

    added target aggregation logic adding code to aggregate TARGET values from all .nessus files previously only the first file's TARGETs were kept this is mostly for documentation purposes to maintain a record of all target ranges

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Nessus Export Merger
 based on https://gist.github.com/btoews/2720173
+
+combine .nessus export files
+Note that scans must be of the same type (same plugins must be used), but they can be from different subnets or different parts of the same subnet.
+
+to run, place nessus_merge.py in the same directory as .nessus files to combin
+python nessus_merge.py
+
--- nessus_merge.py
+++ nessus_merge.py
@@ -1,22 +1,34 @@
 # file: merger.py
 # based off: http://cmikavac.net/2011/07/09/merging-multiple-nessus-scans-python-script/
 # by: mastahyeti
+# https://gist.github.com/btoews/2720173
 
 import xml.etree.ElementTree as etree
 import shutil
 import os
 
 first = 1
+
+# Collect all TARGET values
+aggregated_targets = set() 
+
 for fileName in os.listdir("."):
    if ".nessus" in fileName:
       print(":: Parsing", fileName)
+      
       if first:
          mainTree = etree.parse(fileName)
+         for pref in mainTree.findall(".//ServerPreferences/preference"):
+            if pref.find("name").text == "TARGET":
+               aggregated_targets.update(pref.find("value").text.split(','))
          report = mainTree.find('Report')
          report.attrib['name'] = 'Merged Report'
          first = 0
       else:
          tree = etree.parse(fileName)
+         for pref in tree.findall(".//ServerPreferences/preference"):
+            if pref.find("name").text == "TARGET":
+               aggregated_targets.update(pref.find("value").text.split(','))
          for host in tree.findall('.//ReportHost'):
             existing_host = report.find(".//ReportHost[@name='"+host.attrib['name']+"']")
             if not existing_host:
@@ -28,6 +40,12 @@ for fileName in os.listdir("."):
                         print("adding finding: " + item.attrib['port'] + ":" + item.attrib['pluginID'])
                         existing_host.append(item)
       print(":: => done.")    
+
+target_str = ",".join(sorted(aggregated_targets))
+for pref in mainTree.findall(".//ServerPreferences/preference"):
+    if pref.find("name").text == "TARGET":
+        pref.find("value").text = target_str
+        break
    
 if "nss_report" in os.listdir("."):
    shutil.rmtree("nss_report")
